1f4f29368debcce28653017f28c6d4434ed50a3a
Tauli01~ <taulant.pireva@gmail.com>
2024-06-07 11:28:11 +0200

    added inference

--- backend/inference.py
+++ backend/inference.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot as plt
 # Paths
 BASE_PATH = Path('D:/ZHAW/ML2/ML2project')
 MODEL_PATH = BASE_PATH / 'backend/models/yolov5s_trained.pt'
-IMG_PATH = BASE_PATH / 'path/to/test/image.jpg'  # Update with the path to your test image
+#MODEL_PATH = BASE_PATH / 'backend/models/best.pt'
+IMG_PATH = BASE_PATH / 'backend/test_images/160.jpg'  # Update with the path to your test image
 
 # Load the model
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=str(MODEL_PATH), force_reload=True)
@@ -20,6 +21,8 @@ results = model(img)
 # Display results
 results.show()  # Show results in a window
 results.save(Path('output'))  # Save results to 'output' directory
+results = model(img)
+print(results.xyxy[0])  # Print the raw results to check if detections are made
 
 # Or plot with matplotlib
 plt.imshow(cv2.cvtColor(results.render()[0], cv2.COLOR_BGR2RGB))
